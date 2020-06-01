f39953c37ac93bf57d11cfc8540f167da422b04a
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-01 15:06:04 -0300

    My first react app, and typescript file. Can not describe my hapiness when created the file.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuarios');
+    response.json(['Guilherme', 'Fabiany', 'Javel']);
+});
+
+app.listen(3333);
