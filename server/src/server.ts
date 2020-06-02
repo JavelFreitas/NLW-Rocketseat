4d427fde70ec7be7d105e1a0739fd4adcfed9194
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-02 15:03:50 -0300

    learning routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,4 +7,11 @@ app.get('/users', (request, response) => {
     response.json(['Guilherme', 'Fabiany', 'Javel']);
 });
 
+app.post('/users', (request, response) => {
+    const user = {
+        name: "Javel",
+        email: "javelqueiroz@outlook.com.br"
+    }
+    return response.json(user);
+});
 app.listen(3333);
