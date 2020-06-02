d79865027c707cedaf90b3db1e8a62bdee74ff9b
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-02 17:39:59 -0300

    Created Controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,16 @@
-import express from 'express';
+import express from "express";
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 
-routes.get('/items', (request, response) => {
-    return response.json({message: 'Hello World'});
-});
+routes.get("/items", itemsController.index);
+
+routes.post("/points",pointsController.create);
 
 export default routes;
