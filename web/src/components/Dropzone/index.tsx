fb15acad440799448f57ef87f1d4d4d087cb9649
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-06 14:14:26 -0300

    Added dropzone

--- web/src/components/Dropzone/index.tsx
+++ web/src/components/Dropzone/index.tsx
@@ -1,26 +1,41 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { FiUpload } from 'react-icons/fi'
 
 import './styles.css';
 
+interface Props {
+    onFileUploaded: (file: File) => void;
+}
 
-const Dropzone = () => {
+
+const Dropzone: React.FC<Props> = ({onFileUploaded}) => {
+    const [selectedFileUrl, setSelectedFileUrl] = useState('');
     const onDrop = useCallback(acceptedFiles => {
+        const file = acceptedFiles[0];
 
-    }, [])
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({ 
+        const fileUrl = URL.createObjectURL(file);
+        setSelectedFileUrl(fileUrl);
+        onFileUploaded(file);
+    }, [onFileUploaded])
+    const { getRootProps, getInputProps } = useDropzone({
         onDrop,
-        accept:'image/*'
+        accept: 'image/*'
     })
 
     return (
         <div className='dropzone' {...getRootProps()}>
-            <input {...getInputProps()} />
-            <p>
-                <FiUpload />
-                Imagem do estabelecimento
-            </p>
+            <input {...getInputProps()} accept='image/*' />
+
+            {selectedFileUrl
+                ? <img src={selectedFileUrl} alt='Point thumbnail' />
+                : <p>
+                    <FiUpload />
+                    Imagem do estabelecimento
+                </p>
+            }
+
+
         </div>
     )
 }
