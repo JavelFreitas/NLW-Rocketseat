e8ba00dff9ce5925784d75f2456747c00231f728
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-07 16:33:43 -0300

    Changing localhost

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -22,7 +22,7 @@ class PointsController {
     const serializedPoints = points.map((point) => {
       return {
         ...points,
-        image_url: `http://192.168.1.15:3333/uploads/${point.image}`,
+        image_url: `http://192.168.1.14:3333/uploads/${point.image}`,
       };
     });
 
@@ -40,7 +40,7 @@ class PointsController {
 
     const serializedPoint = {
         ...point,
-        image_url: `http://192.168.1.15:3333/uploads/${point.image}`,
+        image_url: `http://192.168.1.14:3333/uploads/${point.image}`,
     };
 
     const items = await knex("items")
